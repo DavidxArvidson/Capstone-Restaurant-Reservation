93d2098ea5d016adefd6d5c79870cd50d52bf282
David Arvidson <arvidson6@gmail.com>
2022-04-19 08:34:20 -0500

    US-5_Frontend-done

--- front-end/src/dashboard/TableTableComp.js
+++ front-end/src/dashboard/TableTableComp.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-export default function TableTableComp({ table }) {
+export default function TableRow({ table, handleFinish }) {
+	const history = useHistory();
 	if (!table) {
         return null;
     }
+
+	function handleFinish() {
+		if (window.confirm("Are you sure you want to finish this table?")) {
+			history.push("/dashboard");
+		}
+	}
 
 	return (
 		<tr>
@@ -12,6 +20,12 @@ export default function TableTableComp({ table }) {
 			<td>{table.table_name}</td>
 			<td>{table.capacity}</td>
 			<td data-table-id-status={table.table_id}>{table.status}</td>
+
+			{table.status === "occupied" &&
+				<td data-table-id-finish={table.table_id}>
+					<button onClick={handleFinish} type="button">Finish</button>
+				</td>
+			}
 		</tr>
 	);
 }
